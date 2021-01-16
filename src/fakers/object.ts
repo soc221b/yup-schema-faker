dc0b8294e3e0b3c01ffba43672521d3f83b0bdb2
Ernest <iendeavor.github.io@gmail.com>
2021-01-16 20:19:40 +0800

    feat: implement object methods

--- src/fakers/object.ts
+++ src/fakers/object.ts
@@ -1,10 +1,27 @@
+import { random, lorem } from 'faker'
 import { AnySchema, ObjectSchema } from 'yup'
 import { ObjectShape } from 'yup/lib/object'
 import { FakeSchema } from '../type'
 
 export const fakeObject: FakeSchema<ObjectSchema<ObjectShape>> = (schema, fake) => {
-  const fields = schema.fields
-  return Object.keys(fields).reduce((object, key) => {
+  let result = {}
+
+  const fields = Object.keys(schema.fields)
+  result = fields.reduce((object, key) => {
     return Object.assign(object, { [key]: fake(schema.fields[key] as AnySchema) })
-  }, {})
+  }, result)
+
+  const noUnknown = schema.describe().tests.some(test => test.name === 'noUnknown')
+  if (noUnknown === false) {
+    Array(random.number({ min: 10, max: 10 }))
+      .fill(null)
+      .map(() => lorem.word())
+      .filter(field => fields.includes(field) === false)
+      .slice(0, 5)
+      .reduce((object, key) => {
+        return Object.assign(object, { [key]: lorem.paragraph() })
+      }, result)
+  }
+
+  return result
 }
--- test/object.test.ts
+++ test/object.test.ts
@@ -1,6 +1,20 @@
 import { object } from 'yup'
 import { fake } from '../src'
 
-it('should fake data for object schema', () => {
-  expect(Object.prototype.toString.call(fake(object().shape({ a: object().shape({}) })))).toBe('[object Object]')
+it('should works with object', () => {
+  const schema = object().required()
+  const actual = fake(schema)
+  expect(schema.isValidSync(actual)).toBe(true)
+})
+
+it('should works with shape', () => {
+  const schema = object().required().shape({ key: object().required() })
+  const actual = fake(schema)
+  expect(schema.isValidSync(actual)).toBe(true)
+})
+
+it('should works with noUnknown', () => {
+  const schema = object().required().shape({ key: object().required() }).noUnknown()
+  const actual = fake(schema)
+  expect(schema.isValidSync(actual)).toBe(true)
 })
