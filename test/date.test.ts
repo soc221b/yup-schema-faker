0c7e338b18405525dce1a62c361db5fd7a9dcdbb
Ernest <iendeavor.github.io@gmail.com>
2021-01-16 20:42:37 +0800

    feat: implement date methods

--- test/date.test.ts
+++ test/date.test.ts
@@ -7,6 +7,20 @@ it('should works with date', () => {
   expect(schema.isValidSync(actual)).toBe(true)
 })
 
+it('should works with min', () => {
+  const now = new Date().toISOString()
+  const schema = date().min(now)
+  const actual = fake(schema)
+  expect(schema.isValidSync(actual)).toBe(true)
+})
+
+it('should works with max', () => {
+  const now = new Date().toISOString()
+  const schema = date().max(now)
+  const actual = fake(schema)
+  expect(schema.isValidSync(actual)).toBe(true)
+})
+
 it('should works with min, max', () => {
   const now = new Date().toISOString()
   const schema = date().min(now).max(now)
