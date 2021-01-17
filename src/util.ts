a1dda46765e358709f99da4b67c2da95d893ef5d
Ernest <iendeavor.github.io@gmail.com>
2021-01-17 12:06:51 +0800

    fix: should logs on non-production mode

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 const PREFIX_MESSAGE = '[yup-schema-faker]'
 export function warn(message: string) {
-  if (process.env.NODE_ENV !== 'development') {
+  if (__DEV__) {
     console.warn(`${PREFIX_MESSAGE} ${message}`)
   }
 }
