a6cf584156054daee322d2fbcdbfae446c5733d2
Ernest <iendeavor.github.io@gmail.com>
2021-01-22 08:59:49 +0800

    chore: docs flow

--- scripts/docs.sh
+++
@@ -1,9 +0,0 @@
-rm -rf docs
-pwd=$PWD
-
-cd site
-yarn build
-
-cd $pwd
-mv site/dist docs
-sed -i '' 's/assets/yup-schema-faker\/assets/' docs/index.html
--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.vue'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
