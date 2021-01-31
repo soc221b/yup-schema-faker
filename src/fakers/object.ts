16127ecd26755b7412d94d4e8a2bd8fdaef03a09
Ernest <iendeavor.github.io@gmail.com>
2021-01-31 15:38:27 +0800

    fix: reference should be evaluated in the proper order

--- src/fakers/object.ts
+++ src/fakers/object.ts
@@ -4,15 +4,25 @@ import { MixedFaker } from './mixed'
 
 import type { AnySchema, ObjectSchema } from 'yup'
 import type { ObjectShape } from 'yup/lib/object'
+import Reference from 'yup/lib/Reference'
 
 export class ObjectFaker extends MixedFaker<ObjectSchema<ObjectShape>> {
   doFake() {
-    let result = {}
-
     const fields = Object.keys(this.schema.fields)
-    result = fields.reduce((object, key) => {
-      return Object.assign(object, { [key]: ObjectFaker.rootFake(this.schema.fields[key] as AnySchema, result) })
-    }, result)
+    let result = {}
+    result = fields
+      .filter(key => this.schema.fields[key] instanceof Reference === false)
+      .reduce((object, key) => {
+        return Object.assign(object, { [key]: ObjectFaker.rootFake(this.schema.fields[key] as AnySchema, result) })
+      }, result)
+    result = Object.assign(
+      result,
+      fields
+        .filter(key => this.schema.fields[key] instanceof Reference)
+        .reduce((object, key) => {
+          return Object.assign(object, { [key]: ObjectFaker.rootFake(this.schema.fields[key] as AnySchema, result) })
+        }, result),
+    )
 
     const noUnknown = this.schema.describe().tests.some(test => test.name === 'noUnknown')
     if (noUnknown === false) {
--- test/ref.test.ts
+++ test/ref.test.ts
@@ -1,17 +1,37 @@
-import { ref, object } from 'yup'
+import { ref, object, mixed } from 'yup'
 import { fake } from '../src'
 
 it('should works with ref', () => {
   const schema = object()
     .defined()
+    .strict()
     .noUnknown()
     .shape({
-      nested: object().defined().shape({
-        key: object().defined(),
-      }),
-      ref: ref('nested'),
+      siblingRef: ref('value'),
+      value: mixed(),
+      siblingDescRef: ref('desc.value'),
+      desc: object()
+        .defined()
+        .strict()
+        .noUnknown()
+        .shape({
+          siblingRef: ref('value'),
+          value: mixed(),
+          siblingDescRef: ref('desc.value'),
+          desc: object()
+            .defined()
+            .strict()
+            .noUnknown()
+            .shape({
+              siblingRef: ref('value'),
+              value: mixed(),
+            }),
+        }),
     })
+
   const actual = fake(schema)
   expect(schema.isValidSync(actual)).toBe(true)
-  expect(actual.ref).toBe(actual.nested)
+
+  const exp = schema.cast(actual)
+  expect(actual).toBe(exp)
 })
