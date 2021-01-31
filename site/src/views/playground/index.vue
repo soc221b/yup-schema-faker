565991bdc0dba3eea08066e1f91008d3934fc568
Ernest <iendeavor.github.io@gmail.com>
2021-01-31 23:29:56 +0800

    docs: add more complex example

--- site/src/views/playground/index.vue
+++ site/src/views/playground/index.vue
@@ -11,7 +11,7 @@ Wrapper
 
 <script>
 import { defineComponent, reactive } from 'vue'
-import { mixed, bool, boolean, string, number, date, array, object } from 'yup'
+import { mixed, bool, boolean, string, number, date, array, object, ref, lazy } from 'yup'
 import * as yup from 'yup'
 import { fake } from 'yup-schema-faker'
 
@@ -21,26 +21,33 @@ export default defineComponent({
     const defaultCode = `
 // Hint:
 // 1. You could edit this to use your schema
-// 2. You could access \`yup, mixed, bool, boolean, string, number, date, array, object and its methods\`
+// 2. You could access \`yup, mixed, bool, boolean, string, number, date, array, object, ref, lazy and its methods\`
 
-yup.array().length(5).required().of(
-  yup.object()
-    .required()
-    .noUnknown()
-    .shape({
-      name: yup.string().required().min(4).max(20),
-      age: yup.number().required().min(18).max(100).positive().integer(),
-      email: yup.string().nullable().email(),
-      website: yup.string().required().url(),
-    })
-)
+const schema = object()
+  .strict()
+  .required()
+  .noUnknown()
+  .shape({
+    // basic
+    name: string().strict().required().trim(),
+    password: string().required().min(8),
+    emails: array().required().min(1).of(
+      string().defined().email(),
+    ),
+
+    // ref
+    confirmPassword: ref('password'),
+
+    // lazy
+    children: lazy(() => schema.notRequired().default(undefined)),
+  })
     `.trim()
     const custom = reactive({
       fake() {
         const schema = new Function(
-          'yup, mixed, bool, boolean, string, number, date, array, object',
-          '"use strict";return (' + custom.code + ')',
-        )(yup, mixed, bool, boolean, string, number, date, array, object)
+          'yup, mixed, bool, boolean, string, number, date, array, object, ref, lazy',
+          '"use strict"; ' + custom.code + '; return schema;',
+        )(yup, mixed, bool, boolean, string, number, date, array, object, ref, lazy)
         custom.data = fake(schema)
       },
       data: undefined,
