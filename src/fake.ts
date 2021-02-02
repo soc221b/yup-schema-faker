6c82a45112eda544ca9fa809873a80dc7c5aadb7
Ernest <iendeavor.github.io@gmail.com>
2021-02-02 10:31:30 +0800

    refactor: hide implementation

---
+++ src/fake.ts
@@ -0,0 +1,42 @@
+import { random } from 'faker'
+import { AnySchema } from 'yup'
+import { ArrayFaker } from './fakers/array'
+import { BooleanFaker } from './fakers/boolean'
+import { DateFaker } from './fakers/date'
+import { MixedFaker } from './fakers/mixed'
+import { NumberFaker } from './fakers/number'
+import { ObjectFaker } from './fakers/object'
+import { StringFaker } from './fakers/string'
+import { Fake } from './type'
+import { isLazy, isReference } from './util'
+
+MixedFaker.rootFake = rootFake
+
+export const typeToFaker = new Map<String, any>()
+typeToFaker.set('array', ArrayFaker)
+typeToFaker.set('boolean', BooleanFaker)
+typeToFaker.set('date', DateFaker)
+typeToFaker.set('number', NumberFaker)
+typeToFaker.set('object', ObjectFaker)
+typeToFaker.set('string', StringFaker)
+typeToFaker.set('mixed', MixedFaker)
+
+export function rootFake<Schema extends AnySchema>(schema: Schema, parent?: any): ReturnType<Fake<Schema>> {
+  if (isReference(schema)) {
+    return (schema as any).getValue(undefined, parent)
+  }
+  if (isLazy(schema)) {
+    if (random.float({ min: 0, max: 1 }) > 0.1) return undefined as any
+    return rootFake(schema.resolve({}))
+  }
+  if ((schema as any).conditions.length) {
+    return rootFake(schema.resolve({ parent }))
+  }
+
+  const faker = new (typeToFaker.get(schema.type) as typeof MixedFaker)(schema)
+  return faker.fake()
+}
+
+export function fake<Schema extends AnySchema>(schema: Schema) {
+  return rootFake(schema)
+}
--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,6 @@
 import type { AnySchema } from 'yup'
 
-export type Fake<Schema extends AnySchema> = (schema: AnySchema, parent?: any) => ReturnType<Schema['cast']>
+export type Fake<Schema extends AnySchema> = (schema: AnySchema) => ReturnType<Schema['cast']>
 
 export interface FakeSchema<Schema extends AnySchema = AnySchema> {
   (schema: Schema, fake: Fake<AnySchema>): any
