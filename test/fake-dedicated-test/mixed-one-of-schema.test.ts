083379cdb6d945d3b12ed204996f7d34590dcc71
Ernest <iendeavor.github.io@gmail.com>
2021-03-06 19:11:57 +0800

    test: refactor

--- test/fake-dedicated-test/mixed-one-of-schema.test.ts
+++ test/fake-dedicated-test/mixed-one-of-schema.test.ts
@@ -5,23 +5,23 @@ import type { AnySchema } from 'yup'
 
 declare module 'yup' {
   interface BaseSchema {
-    some<Schema extends AnySchema>(this: Schema, ...schemas: AnySchema[]): Schema
+    oneOfSchema<Schema extends AnySchema>(this: Schema, schemas: AnySchema[]): Schema
   }
 }
 
-addMethod(mixed, 'some', function (...schemas) {
+addMethod(mixed, 'oneOfSchema', function (schemas) {
   // eslint-disable-next-line no-template-curly-in-string
   const message = '${path} is not a correct type'
 
   return this.test({
     message,
-    name: 'some',
+    name: 'oneOfSchema',
     exclusive: true,
     params: {
       schemas,
     },
     test(value) {
-      return schemas.some(schema => schema.isValidSync(value))
+      return schemas.some((schema: AnySchema) => schema.isValidSync(value))
     },
   })
 })
@@ -29,34 +29,36 @@ addMethod(mixed, 'some', function (...schemas) {
 it('should be a valid schema', () => {
   expect(
     mixed()
-      .some(mixed().oneOf([42]), mixed().oneOf(['foo']))
+      .oneOfSchema([mixed().oneOf([42]), mixed().oneOf(['foo'])])
       .isValidSync('foo'),
   ).toBe(true)
 
   expect(
     mixed()
-      .some(mixed().oneOf([42]), mixed().oneOf(['foo']))
+      .oneOfSchema([mixed().oneOf([42]), mixed().oneOf(['foo'])])
       .isValidSync('bar'),
   ).toBe(false)
 })
 
 it('should be allowed to add dedicated test', () => {
-  fakeDedicatedTest(mixed, 'some', schema => {
-    const innerSchema = schema.tests.find(test => test.OPTIONS.name === 'some')?.OPTIONS.params?.schema as AnySchema
+  fakeDedicatedTest(mixed, 'oneOfSchema', schema => {
+    const innerSchema = schema.tests.find(test => test.OPTIONS.name === 'oneOfSchema')?.OPTIONS.params
+      ?.schema as AnySchema
     return fake(innerSchema)
   })
 })
 
 it('should run dedicated test', () => {
-  fakeDedicatedTest(mixed, 'some', schema => {
-    const innerSchemas = schema.tests.find(test => test.OPTIONS.name === 'some')?.OPTIONS.params?.schemas as AnySchema[]
+  fakeDedicatedTest(mixed, 'oneOfSchema', schema => {
+    const innerSchemas = schema.tests.find(test => test.OPTIONS.name === 'oneOfSchema')?.OPTIONS.params
+      ?.schemas as AnySchema[]
     const pickedSchema = innerSchemas[Math.floor(Math.random() * innerSchemas.length)]
     return fake(pickedSchema)
   })
 
   const innerSchema1 = mixed().oneOf([42])
   const innerSchema2 = mixed().oneOf(['foo'])
-  const schema = mixed().some(innerSchema1, innerSchema2)
+  const schema = mixed().oneOfSchema([innerSchema1, innerSchema2])
   const actual = fake(schema)
   expect(schema.isValidSync(actual)).toBe(true)
 })
