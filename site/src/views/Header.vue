1c79a64f8c2b108a697b798bee0f675f70cec7e1
Ernest <iendeavor.github.io@gmail.com>
2021-06-05 12:50:34 +0800

    docs: update

--- site/src/views/Header.vue
+++ site/src/views/Header.vue
@@ -4,7 +4,7 @@ header.text-black
     span Yup Schema Faker
       sub {{ VERSION }}
     .float-right
-      a.mr-1(href='https://github.com/iendeavor/yup-schema-faker') GitHub
+      a.mr-1(href='https://github.com/iendeavor/yup-schema-faker' target="_blank") GitHub
   .mt-1
     button.mb-1.mr-1.inline-block.border.border-gray-400.rounded.px-2(@click='reloadToResetSeedValue') Reload to reset seed value
     button.mb-1.inline-block.border.border-gray-400.rounded.px-2(@click='reloadToGenerateNewSeedValue') Reload to generate new seed value
