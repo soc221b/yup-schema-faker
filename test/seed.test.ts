88123e2e63c12758c23f7732c6191e5c94f7e560
Ernest <iendeavor.github.io@gmail.com>
2022-04-02 09:11:42 +0800

    refactor: rename

---
+++ test/seed.test.ts
@@ -0,0 +1,45 @@
+import { seed, fake } from '../src'
+import * as yup from 'yup'
+
+const schemas = [
+  yup.mixed(),
+
+  yup.boolean(),
+
+  yup.number(),
+
+  yup.date(),
+
+  yup.string(),
+  yup.string().uuid(),
+  yup.string().email(),
+  yup.string().url(),
+  yup.string().matches(/\d{10}/),
+
+  yup.object().shape({
+    key: yup.string(),
+  }),
+
+  yup.array().of(yup.string()),
+]
+
+describe('seed', () => {
+  it('should produce same results', () => {
+    for (const schema of schemas) {
+      seed(123)
+      const firstRandom = fake(schema)
+
+      seed(123)
+      const secondRandom = fake(schema)
+
+      try {
+        expect(firstRandom).toEqual(secondRandom)
+      } catch (error) {
+        console.log(`schema: ${JSON.stringify(schema.describe(), null, 2)}`)
+        console.log(`first: ${JSON.stringify(firstRandom, null, 2)}`)
+        console.log(`second: ${JSON.stringify(secondRandom, null, 2)}`)
+        throw error
+      }
+    }
+  })
+})
